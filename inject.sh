74c18518af60a4397615c6fea52e53dbca8fc846
Mufanc <mufanc.xyz@gmail.com>
2023-01-27 16:07:09 +0800

    refactor: change injected library path

--- inject.sh
+++ inject.sh
@@ -1,12 +1,12 @@
 PID="$1"
 APK_PATH=$(pm path $(cat /proc/$PID/cmdline) | sed 's/package://')
-LIB_PATH=$(dirname "$APK_PATH")/lib/arm64/libhack.so
+LIB_PATH=$(dirname "$APK_PATH")/lib/hook/libhack.so
 mkdir -p $(dirname "$LIB_PATH")
 cp hack.so "$LIB_PATH"
-chmod 777 "$LIB_PATH"
+chmod 666 "$LIB_PATH"
 logcat -c
 ./injector $PID "$LIB_PATH"
 pmap "$PID" | grep libhack.so
 sleep 3
-rm "$LIB_PATH"
+rm -r $(dirname "$LIB_PATH")
 logcat --pid "$PID" -v raw -v color
